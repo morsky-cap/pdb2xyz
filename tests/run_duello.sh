8b9b80a5f4fb43849c03bd9d658200e1c539fb18
Anze Bozic <anze.bozic@ijs.si>
2025-09-28 23:29:43 +0200

    Large update

--- tests/run_duello.sh
+++ tests/run_duello.sh
@@ -3,4 +3,4 @@
 INFILE=$1
 C0=$2
 
-duello scan -1 ${INFILE}.xyz -2 ${INFILE}.xyz --rmin 50 --rmax 152 --dr 2.0 --resolution 0.6 --top ${INFILE}.yaml --molarity $C0 --cutoff 1000.0 --pmf ${INFILE}_c0${C0}_pmf.dat
+duello scan -1 ${INFILE}.xyz -2 ${INFILE}.xyz --rmin 50 --rmax 151 --dr 1.0 --resolution 0.6 --top ${INFILE}.yaml --molarity $C0 --cutoff 1000.0 --pmf ${INFILE}_c0${C0}_pmf.dat
