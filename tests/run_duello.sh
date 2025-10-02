f0bab0665de47aa49f2e4e109ee46dfc2d342607
morsky-cap <anze.bozic@ijs.si>
2025-10-02 12:23:12 +0200

    Removed test folder

--- tests/run_duello.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-INFILE=$1
-C0=$2
-
-duello scan -1 ${INFILE}.xyz -2 ${INFILE}.xyz --rmin 50 --rmax 151 --dr 1.0 --resolution 0.6 --top ${INFILE}.yaml --molarity $C0 --cutoff 1000.0 --pmf ${INFILE}_c0${C0}_pmf.dat
